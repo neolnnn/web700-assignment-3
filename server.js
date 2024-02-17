7f5dfdc50feb1b949027b29805e803f6f2885870
neolnnn <nathanbernas29@gmail.com>
2024-02-17 17:31:28 -0500

    so many files updated

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+/********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+*  of this assignment has been copied manually or electronically from any other source
+*  (including 3rd party web sites) or distributed to other students.
+*
+*  Name: Nate Joshua Student ID: njoshua2 Date: 2/15/2024
+*
+********************************************************************************/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
-const collegedata = require('./modules/collegeData');
+const collegedata = require('./modules/collegeData'); // imports the collegedata module
 app.use(express.static('views'));
 
-collegedata.initialize().then(() => {
+collegedata.initialize().then(() => { // initializes collegedata before starting the server
     app.get('/', (req, res) => {
-        res.sendFile(`${__dirname}/views/home.html`);
+        res.sendFile(`${__dirname}/views/home.html`); // uses an html file for the home page
     })
 
     app.get('/about', (req, res) => {
@@ -15,20 +25,20 @@ collegedata.initialize().then(() => {
     })
 
     app.get('/htmldemo', (req, res) => {
-        res.sendFile(`${__dirname}/views/htmldemo.html`)
+        res.sendFile(`${__dirname}/views/htmldemo.html`);
     })
 
     app.get('/students', (req, res) => {
         var course = req.query.course;
 
         if (course) {
-            collegedata.getStudentsByCourse(course).then((studentlist) => {
+            collegedata.getStudentsByCourse(course).then((studentlist) => { // calls this function if there is a query
                 res.send(studentlist);
             }).catch(() => {
                 res.json({message: "no results"});
             })
         } else {
-            collegedata.getAllStudents().then((studentlist) => {
+            collegedata.getAllStudents().then((studentlist) => { // otherwise calls this function
                 res.send(studentlist);
             }).catch(() => {
                 res.json({message: "no results"});
@@ -62,11 +72,11 @@ collegedata.initialize().then(() => {
         })
     })
 
-    app.use((req, res) => {
-        res.status(404).send("<h1>404 Error LMAO</h1>")
+    app.use((req, res) => { // shows a 404 page if an invalid page is entered
+        res.status(404).sendFile(`${__dirname}/views/errorpage.html`);
     })
 }).then(() => {
-    app.listen(HTTP_PORT, () => {console.log(`Server listening on port ${HTTP_PORT}`)});
+    app.listen(HTTP_PORT, () => {console.log(`Server listening on port ${HTTP_PORT}`)}); // starts the server
 }).catch((err) => {
-    console.error(`Error importing data, server not started: ${err}`)
+    console.error(`Error importing data, server not started: ${err}`);
 })
